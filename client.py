55b928d6dbd7e67e7553fc76a0439e50d83f79e5
Suresh Nayak <sureshnnayak24@gmail.com>
2023-03-11 16:26:54 -0700

    NTP client server code

--- client.py
+++ client.py
@@ -1,11 +1,46 @@
-import socket
-UDP_IP_ADDRESS = "127.0.0.1"
-UDP_PORT_NO = 1234
-Message = "Hello, Server".encode()
+#!/usr/bin/env python
+from contextlib import closing
+from socket import socket, AF_INET, SOCK_DGRAM
+import struct
+import datetime
+import time
 
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+NTP_PACKET_FORMAT = "!12I"
+NTP_DELTA = 2208988800  # 1970-01-01 00:00:00
+NTP_QUERY = b'\x1b' + 47 * b'\0'  
 
-while True:
-    msg = str(input("Enter your message: "))
-    msg = msg.encode()
-    clientSock.sendto(msg, (UDP_IP_ADDRESS, UDP_PORT_NO))
+
+def getTime():
+    t = datetime.datetime.now().timestamp() + NTP_DELTA
+    arr = str(t).split('.')
+    return (int(arr[0]), int(arr[1]))
+
+
+
+"""
+T1  is  Origin  Timestamp field  of  the  response  message;
+T2  is  the  Receive Timestamp field  of  the response message;  
+T3  is  the  Transmit Timestamp field  of  the  response  message;  and  
+T4  is  the  time  this response  message arrived
+"""
+
+def get_ntp_time(host="pool.ntp.org", port=123):
+    client = socket(AF_INET, SOCK_DGRAM)
+    client_origin_timestamp_secs, client_origin_timestamp_ms = getTime()
+    client_msg = b'\x1b' + 47 * b'\0'  # NTP request packet
+    client.sendto(client_msg, (host, port))
+  
+    data, address = client.recvfrom(1024)
+    if data:
+        unpacked = struct.unpack(NTP_PACKET_FORMAT,data)
+        T1 =  str(client_origin_timestamp_secs) +"."+ str(client_origin_timestamp_secs)
+        T2 = str(unpacked[8]) +"."+ str(unpacked[9])
+        T3 = str(unpacked[10]) +"."+ str(unpacked[11])
+        return T1,T2,T3
+
+
+if __name__ == "__main__":
+    #print (time.ctime(ntp_time()))
+    print(get_ntp_time("127.0.0.1", 1234))
+    print("\nTime from NTP Server")
+    print(get_ntp_time())
--- server.py
+++ server.py
@@ -1,16 +1,40 @@
-import socket
+#import socket
+from socket import socket, AF_INET, SOCK_DGRAM
+from contextlib import closing
+import datetime
+import struct
+
+
+NTP_PACKET_FORMAT = "!12I"
+
+"""
+In the context of Network Time Protocol (NTP), NTP_DELTA refers to the difference between the Unix 
+epoch time (January 1, 1970) and the NTP epoch time (January 1, 1900). This value is subtracted 
+from the NTP timestamp to convert it into Unix epoch time.
+"""
+NTP_DELTA = 2208988800  # 1970-01-01 00:00:00
+NTP_QUERY = b'\x1b' + 47 * b'\0' 
+
 # Here we define the UDP IP address as well as the port number that we have 
 # already defined in the client python script.
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 1234
 
-# declare our serverSocket upon which
-# we will be listening for UDP messages
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# One difference is that we will have to bind our declared IP address
-# and port number to our newly declared serverSock
+serverSock = socket(AF_INET, SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+
+
+def getTime():
+    t = datetime.datetime.now().timestamp() + NTP_DELTA
+    arr = str(t).split('.')
+    return (int(arr[0]), int(arr[1]))
+    
 
 while True:
     data, addr = serverSock.recvfrom(1024)
-    print ("Message: " + data.decode())
+    server_receipt_timestamp_secs, server_receipt_timestamp_ms = getTime()
+    print("system time :", getTime())
+    server_send_timestamp_secs, server_send_timestamp_ms = getTime()
+    severresponse = struct.pack("!12I", 0, 0, 0, 0, 0, 0, 0, 0, server_receipt_timestamp_secs, server_receipt_timestamp_ms, server_send_timestamp_secs, server_send_timestamp_ms)
+    serverSock.sendto(severresponse, addr)
